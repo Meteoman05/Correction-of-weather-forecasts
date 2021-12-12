72000f025ef30fdffe24c2faba28b8073d81fe01
Meteoman05 <76489240+Meteoman05@users.noreply.github.com>
2021-12-12 11:11:01 +0200

    Add files via upload

---
+++ untitled-1.py
@@ -0,0 +1,23 @@
+import numpy as np
+from metpy.units import units
+import metpy.calc
+
+lat = np.linspace(55.7, 55.8, 3)
+lon = np.linspace(37.6, 37.7, 3)
+dx, dy = metpy.calc.lat_lon_grid_deltas(lat, lon)
+
+a, b, c = 273, -5, 8
+
+pottemp = np.array([[a, a, a],
+                    [a, a, a],
+                    [a, a, a]]) * units.kelvin
+
+u = np.array([[b, b, b],
+              [b, b, b],
+              [b, b, b]]) * units('m/s')
+
+v = np.array([[c, c, c],
+              [c, c, c],
+              [c, c, c]]) * units('m/s')
+
+print(metpy.calc.frontogenesis(pottemp, u, v, dx=dx, dy=dy)); print('Heeeeeeeeeeeeeellooooooooooooooooooooooo')
